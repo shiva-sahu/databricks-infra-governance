e9a60612a2861d83bd9979757c48a2131963ecdb
shiva-gcw <SSahu@gerberchildrenswear.com>
2026-05-18 22:49:50 +0530

    fvevv

--- terraform/environments/prod/admins.tf
+++ terraform/environments/prod/admins.tf
@@ -3,12 +3,3 @@
 resource "databricks_user" "shiva" {
   user_name = "shiva-sahu@v4ctscoutlook.onmicrosoft.com"
 }
-
-data "databricks_group" "admins" {
-  display_name = "admins"
-}
-
-resource "databricks_group_member" "shiva_admin" {
-  group_id  = data.databricks_group.admins.id
-  member_id = databricks_user.shiva.id
-}
